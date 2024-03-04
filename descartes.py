c41935c1d04720862a276fa67657ae6ece28f50b
xiaoming-01ai <144644792+xiaoming-01ai@users.noreply.github.com>
2024-03-04 13:55:01 +0800

    Update descartes.py

--- descartes.py
+++ descartes.py
@@ -1,1 +1,15 @@
+import py01ai
+import numpy as np
+
+
+n, d = 10000, 128
+X = np.random.randn(n, d)
+
+index = py01ai.FNGIndex(metric="square_l2", dimension=dimension))
+index.init(count=1000000, M=32, S=1, L=200)
+index.add_vector(X)
+
+v = np.random.randn(1, 128)
+results = index.search(query=v, topk=10)
+print(results)
 
